b96472c8312edfbeb3ef95fae0377606fb1d6414
Braden Polly <bpolly@covermymeds.com>
2019-03-22 15:28:11 -0400

    Better responses

--- app.py
+++ app.py
@@ -15,12 +15,14 @@ def handle_data():
     html = urlopen(provided_link).read().decode('utf-8')
     mp4_match = re.search('videoUri\": \"(.*.mp4)', html)
     if mp4_match != None:
-        return(mp4_match.group(1))
+        return json.dumps({ 'links': [mp4_match.group(1)] })
     else:
         feed_url_match = re.search("feedURL = '(.*)'", html)
         json_link = feed_url_match.group(1)
         if feed_url_match == None:
-            return('Could not find file list. Exiting')
+            response = json.dumps({ 'error': 'Could not find file list. Exiting' })
+            response.status_code = 500
+            return response
         else:
             video_id = re.search("contentId: '(.*)'", html).group(1)
             json_file_contents = json.loads(urlopen(json_link).read().decode('utf-8'))
